46c65deaa3f3fc110b975698c8c6c7f34c1db722
Evans93 <evansehis7837@gmail.com>
2024-11-13 12:59:52 +0100

    t

---
+++ week2.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import numpy as np
+
+
+#make this example reproducible 
+
+np.random.seed (1)
+
+#create DataFrame
+
+df = pd.DataFrame({'team': np.repeat(['A', 'B', 'C'], 100),
+           'points': np.random.normal(loc=20, scale=2, size=300)})
+
+#view head of DataFrame
+print(df.head())
+
+#create histogram with 20 bins
+df.plot.hist(column=['points'], edgecolor='black', bins=87)
+plt.show()
