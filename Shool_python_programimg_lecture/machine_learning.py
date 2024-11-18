a7ab71c9a9bb8db8273da949b7143c56fe879824
Evans93 <evansehis7837@gmail.com>
2024-11-18 14:56:19 +0100

    testing new python skills

---
+++ Shool_python_programimg_lecture/machine_learning.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#define data
+
+x = np. array([50,33,110,45,66,87,42,37,73,95])
+y = np. array ([100,62,180,70,84,93,50,52,90,120])
+
+#find Line of best fat
+
+a, b = np. polyfit(x, y, 1)
+
+#add  points to plot
+
+plt.scatter (x, y, color='purple')
+
+#add
+
+## add line of best fit to plot
+
+plt.plot(x, a*x+b, color='steelblue', linestyle ='--', linewidth=2)
+         
+## add fittea regression equation to pLot
+
+plt. text(1, 17, 'y = '+ '{:.2f}'.format(b) + ' + {:.2f} '.format(a) + 'x', size=14)
+
+plt. show()
